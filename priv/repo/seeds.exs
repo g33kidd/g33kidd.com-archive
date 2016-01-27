43456d13bd71ec48ee9146b9c77f141d83477044
Joshua Kidd <kidd.josh.343@gmail.com>
2016-01-27 07:42:34 -0700

    New styles for new design

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,7 +11,19 @@
 # and so on) as they will fail if something goes wrong.
 
 Blog.Repo.insert!(%Blog.Post{
-  title: "Creating a Storage Module for OAuth2Ex",
-  content: "",
-  slug: ""
+  title: "Hello, World!",
+  content: """
+Hello, World!
+
+No, this isn’t a programming tutorial, but I have thought about doing that sometime. This post is for welcoming you to my new website! I finally decided to take the time to sit down and put together a website for myself and you guys. Not only is this a website for me, but it’s also a website where I will give back to everyone reading and visiting. I’ll be posting tips/tricks for WordPress and some various programming languages, plus some stuff about my favorite frameworks to work with.
+
+Intro
+
+Just a little bit about me. I was born in Colorado and raised in Kansas where I would eventually learn about computer science and programming. About a few months ago I stayed in LA for a little bit and then moved to Colorado to work for Folsom Creative. Mainly I work with WordPress, PHP, Less, and JavaScript. When I’m just messing around with this I use Ruby, Sass, Node, Elixir, and GO. I love coming up with ideas and building them in code.
+
+I hope you enjoy seeing all the cool stuff I have to offer.
+
+See ya next time!
+  """,
+  slug: "hello-world"
 })
--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -2,7 +2,8 @@ defmodule Blog.PostController do
   use Blog.Web, :controller
 
   def index(conn, _) do
-    render conn, "index.html"
+    render conn, "index.html",
+      posts: Blog.Repo.all Blog.Post
   end
 
   def show(conn, %{"slug" => slug}) do
